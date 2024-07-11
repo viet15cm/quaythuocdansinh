8f422a4857f7669163614403ea9126da33564008
viet15cm <viet99cm@gmail.com>
2024-07-11 18:11:46 +0700

    create

---
+++ js/index.js
@@ -0,0 +1,17 @@
+window.onscroll = function () {myFunction()};
+var navbar = document.getElementById("navbar-search");
+
+var mainhtm = document.getElementById("main");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+
+if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+    mainhtm.classList.add("magin");
+    } 
+    else {
+    navbar.classList.remove("sticky");
+    
+  }
+}
